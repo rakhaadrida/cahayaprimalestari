b2465e9ab76b313f7c02e4d9cb45327b25a82b8a
rakhaadridaa <62930338+rakhaadrida@users.noreply.github.com>
2021-12-10 15:54:36 +0700

    adjust data double

--- app/Http/Controllers/GudangController.php
+++ app/Http/Controllers/GudangController.php
@@ -21,7 +21,7 @@ class GudangController extends Controller
     public function index()
     {
         AccReceivable::query()
-            ->whereIn('id_so', ['IV21100359', 'IV21111197', 'IV21080554', 'IV21110570', 'IV21110374', 'IV21090863'])
+            ->whereIn('id_so', ['IV21100302'])
             ->where('keterangan', 'BELUM LUNAS')
             ->delete();
 
