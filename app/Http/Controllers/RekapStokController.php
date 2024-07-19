8c7e153ad9f28cf284c6234b961864ad5f640fce
rakhaadrida <62930338+rakhaadrida@users.noreply.github.com>
2024-07-19 16:36:23 +0700

    [feat] remove unused code

--- app/Http/Controllers/RekapStokController.php
+++ app/Http/Controllers/RekapStokController.php
@@ -25,17 +25,12 @@ class RekapStokController extends Controller
     public function index() {
         $jenis = JenisBarang::All();
         $gudang = Gudang::All();
-        // $stokGd = StokBarang::join('barang', 'barang.id', 'stok.id_barang')
-        //           ->select('id_gudang')->selectRaw('sum(stok) as stok')
-        //           ->where('id_kategori', 'KAT04')->where('stok', '!=', 0)->groupBy('id_gudang')->get();
-
-        // return response()->json($stokGd);
 
         $data = [
             'jenis' => $jenis,
             'gudang' => $gudang,
         ];
-        
+
         return view('pages.laporan.rekapstok.index', $data);
     }
 
@@ -95,11 +90,11 @@ class RekapStokController extends Controller
             $j->{'total'} = $brg + $sub;
         }
 
-        $el = 0; $k = $jenis->count(); 
+        $el = 0; $k = $jenis->count();
         foreach($jenis as $j) {
             $gabung = 0;
             if($j->total <= 80) {
-                for($i = $el+1; $i < $k; $i++) { 
+                for($i = $el+1; $i < $k; $i++) {
                     if($jenis[$el]->total + $jenis[$i]->total <= 127) {
                         // if($jenis[$i]->nama != 'PRIME') {
                             $jenis[$el]->total += $jenis[$i]->total;
@@ -111,12 +106,12 @@ class RekapStokController extends Controller
                                 return $item->id != $kode;
                             });
                         // }
-                        $gabung++;   
+                        $gabung++;
                     }
                 }
                 $jenis = $jenis->values();
-                $k -= $gabung;   
-            } 
+                $k -= $gabung;
+            }
             $el++;
         }
 
@@ -154,11 +149,11 @@ class RekapStokController extends Controller
             $j->{'total'} = $brg + $sub;
         }
 
-        $el = 0; $k = $jenis->count(); 
+        $el = 0; $k = $jenis->count();
         foreach($jenis as $j) {
             $gabung = 0;
             if($j->total <= 80) {
-                for($i = $el+1; $i < $k; $i++) { 
+                for($i = $el+1; $i < $k; $i++) {
                     if($jenis[$el]->total + $jenis[$i]->total <= 127) {
                         // if($jenis[$i]->nama != 'PRIME') {
                             $jenis[$el]->total += $jenis[$i]->total;
@@ -170,12 +165,12 @@ class RekapStokController extends Controller
                                 return $item->id != $kode;
                             });
                         // }
-                        $gabung++;   
+                        $gabung++;
                     }
                 }
                 $jenis = $jenis->values();
-                $k -= $gabung;   
-            } 
+                $k -= $gabung;
+            }
             $el++;
         }
 
