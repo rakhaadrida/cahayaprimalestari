870778f617057c0f40c48ab7c13d6b234d079389
rakhaadrida <62930338+rakhaadrida@users.noreply.github.com>
2025-03-26 12:15:57 +0700

    revert readonly in sales order

--- app/Http/Controllers/SalesOrderController.php
+++ app/Http/Controllers/SalesOrderController.php
@@ -158,8 +158,8 @@ class SalesOrderController extends Controller
         $lastnumber = (int) substr($lastcode[0]->id, 6, 4);
         $lastnumber++;
         $newcode = 'IV'.$tahun.$bulan.sprintf('%04s', $lastnumber);
-        // $kode = $newcode;
-        $kode = $request->kode;
+        $kode = $newcode;
+        // $kode = $request->kode;
 
         $statusHal = $status;
 
