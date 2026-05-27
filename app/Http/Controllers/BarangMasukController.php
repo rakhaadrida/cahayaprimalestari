fdc434b11baed7f1df634c5b264e4564e73fe5d0
Rakha' Adrida Bagaspati <62930338+rakhaadrida@users.noreply.github.com>
2026-05-27 23:25:30 +0700

    [feat] adjust goods receipt

--- app/Http/Controllers/BarangMasukController.php
+++ app/Http/Controllers/BarangMasukController.php
@@ -100,7 +100,7 @@ class BarangMasukController extends Controller
             $lastnumber = (int) substr($lastcode[0]->id, 6, 4);
             $lastnumber++;
             $newcode = 'AP'.$tahun.$bulan.sprintf('%04s', $lastnumber);
-            $apKode = $newNumber;
+            $apKode = $newcode;
 
             $items = AccPayable::where('id_bm', $request->kode)->count();
             if($items != 1) {
